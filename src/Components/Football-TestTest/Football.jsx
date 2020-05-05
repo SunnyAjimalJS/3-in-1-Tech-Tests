58bc5dce91209b67ddf3e4193d6569044283f667
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-05-05 17:12:26 +0100

    Quiz tech test completed

--- src/Components/Football-TestTest/Football.jsx
+++ src/Components/Football-TestTest/Football.jsx
@@ -1,11 +1,41 @@
-import React, { Component } from 'react';
+/* 
+## Football Team Organiser
+Using the data set provided, build a component to allow users to select players from the list and create their ideal Borussia Mönchengladbach starting eleven.
 
-export default class Football extends Component {
-    render(){
-        return(
-        <>
-        <p>Football Page Works</p>
-        </>
+#### Keep in mind:
+
+* A user can’t insert a player more than once.
+* A team can have no more than 1 Goalkeeper
+* The starting eleven needs to include 3 Defenders, 4 Midfielders, 3 Attackers.
+
+
+The UI should reflect the user choices.
+
+---
+
+*/
+
+// 1. Import the list of data from the JSON file
+// 2. Display these players in a list format
+// 3.  
+
+
+
+import React, { Component } from 'react'
+import data from "./football-team.data.json"
+
+export default class Football extends Component{ 
+    state = {
+        playersList: data
+    }
+    render() {
+        console.log(data);
+        console.log(this.state.playersList)
+        console.log(this.state.playersList[0])
+        return (
+            <div>
+                <h1>Football Team Picker</h1>
+            </div>
         )
     }
 }
--- src/Components/Quiz-TechTest/Quiz.jsx
+++ src/Components/Quiz-TechTest/Quiz.jsx
@@ -1,7 +1,69 @@
-import React, { Component } from 'react'
+/* ## One Question Quiz
+
+* Create a one question quiz that validates itself and checks the user input against the correct answer. 
+Eg. pizza validates true when typed in an input */
+
+import styles from "./Quiz.module.scss";
+import React, { Component } from "react";
+
 
 export default class Quiz extends Component {
-    render(){
-        return(<p>Quiz Page Works!</p>)
+  state = {
+    questions: [
+      {
+        id: 1,
+        question: "Which of these foods is traditionally made round",
+        answer_a: "Spaghetti",
+        answer_b: "Pizza",
+        answer_c: "Lasagna",
+        answer_d: "All of the above",
+        correct_answer: "Pizza",
+      }
+    ], 
+    value: ''
+  }
+
+  handleInputChange = (event) => {
+    this.setState({value: event.target.value});
     }
+
+  checkAnswer(){
+    if(this.state.value === this.state.questions[0].correct_answer){
+      alert("Your answer was " + this.state.value + " - Your answer is correct!")
+    } else{ 
+      alert("Your answer was " + this.state.value + " - Wrong Answer!")
+    }
+  }
+
+  handeSubmit = (e) => {
+    e.preventDefault();
+    this.checkAnswer()
+  }
+ 
+  render() {
+    return (
+      <main>
+        <div className={styles.container}>
+          <h1>{this.state.questions[0].question}</h1>
+          <p>A: {this.state.questions[0].answer_a}</p>
+          <p>B: {this.state.questions[0].answer_b}</p>
+          <p>C: {this.state.questions[0].answer_c}</p>
+          <p>D: {this.state.questions[0].answer_d}</p>
+          <form onSubmit={this.handeSubmit} className={styles.form}>
+            <input
+              className={styles.answer}
+              type="text"
+              placeholder="Type your answer e.g Spaghetti or Pizza etc."
+              onChange={this.handleInputChange}
+            />
+            <input
+              className={styles.btn}
+              type="submit"
+              value="Submit your answer!"
+            />
+          </form>
+        </div>
+      </main>
+    );
+  }
 }
