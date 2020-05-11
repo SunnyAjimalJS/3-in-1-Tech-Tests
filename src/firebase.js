578811615e3787afe869298e60f43cc885e03e9d
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-05-11 16:28:45 +0100

    Firebase added for Deployment

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAQCk6xupnJPYtWbgchjdnFI8ldd3jOJaA",
+    authDomain: "three-tech-tests.firebaseapp.com",
+    databaseURL: "https://three-tech-tests.firebaseio.com",
+    projectId: "three-tech-tests",
+    storageBucket: "three-tech-tests.appspot.com",
+    messagingSenderId: "245944160786",
+    appId: "1:245944160786:web:1bc15bbf8b5a294a14fecb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
